d76781e6e033b824e48d468b8a6b561b033ca743
AmurTigro <martiohusein27@gmail.com>
2024-09-15 17:51:54 +0700

    Created posts module and controller with providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/providers/users.service';
+import { PostsModule } from './posts/posts.module';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/providers/posts.service';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService, UsersService],
+  imports: [UsersModule, PostsModule],
+  controllers: [AppController, PostsController],
+  providers: [AppService, UsersService, PostsService],
 })
 export class AppModule {}
