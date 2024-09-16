834373b35225b2e56fa9a95f1ff2c5a15552bb53
AmurTigro <martiohusein27@gmail.com>
2024-09-16 13:16:48 +0700

    Created api doc for posts

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -15,17 +15,27 @@ import { postType } from '../enums/post-type.enum';
 import { postStatus } from '../enums/post-status.enum';
 import { Type } from 'class-transformer';
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty({
+    example: 'My First Post',
+    description: 'The title of the post',
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: postType,
+    description: 'Possible values, "post", "page", "story", "series"',
+  })
   @IsEnum(postType)
   @IsNotEmpty()
   postType: postType;
 
+  @ApiProperty({ example: 'my-first-post', description: 'The slug' })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -34,33 +44,76 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    enum: postStatus,
+    description: 'Possible values, "draft", "scheduled", "review", "published"',
+  })
   @IsEnum(postStatus)
   @IsNotEmpty()
   status: postStatus;
 
+  @ApiPropertyOptional({
+    example: 'This is my first post',
+    description: 'The content of the post',
+  })
   @IsString()
   @IsOptional()
   content?: string;
+
+  @ApiPropertyOptional({
+    description:
+      'Serialize your JSON object else a validation error will be thrown',
+    example: '{ "title": "Sample Schema" }',
+  })
   @IsJSON()
   @IsOptional()
   schema?: string;
+
+  @ApiPropertyOptional({
+    example: 'https://example.com/featured-img.png',
+    description: 'The url of the featured image',
+  })
   @IsUrl()
   @IsOptional()
   featuredImgUrl?: string;
 
+  @ApiPropertyOptional({ example: '2022-01-01', description: 'Publish on' })
   @IsISO8601()
   @IsOptional()
   publishOn?: Date;
 
+  @ApiPropertyOptional({
+    example: ['nestjs', 'blog'],
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[];
 
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: {
+          type: 'string',
+          description:
+            'This can be any meta identifier in your app meta option',
+          example: 'sidebarEnabled',
+        },
+        value: {
+          type: 'any',
+          description: 'This can be any meta value in your app meta option',
+          example: true,
+        },
+      },
+    },
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionsDto)
-  metaOptions: CreatePostMetaOptionsDto[];
+  metaOptions?: CreatePostMetaOptionsDto[];
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -13,8 +14,27 @@ export class PostsController {
     return this.postsService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Create a new post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your post created successfully',
+  })
   @Post()
   createPost(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
+  }
+
+  @ApiOperation({
+    summary: 'Update a post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'You get a 200 response if your post updated successfully',
+  })
+  @Patch()
+  patchPost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
